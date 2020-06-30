0b8141f426a69215ebac67f1bdd73548572ad1dd
Montrez <montrez.cox@gmail.com>
2020-06-30 18:36:49 -0400

    First commit

---
+++ src/test/java/helloSpring/spring/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package helloSpring.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
